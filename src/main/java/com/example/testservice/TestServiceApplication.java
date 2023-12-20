952600b4313f917677d4dab0ab033651a63884d7
0x4E43 <nimaic.dev@gmail.com>
2023-12-20 15:39:22 +0530

    +test

---
+++ src/main/java/com/example/testservice/TestServiceApplication.java
@@ -0,0 +1,35 @@
+package com.example.testservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@RestController
+public class TestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestServiceApplication.class, args);
+	}
+
+
+	@GetMapping(path = "/test/test-app")
+	public String testApp(){
+		return "App working fine";
+	}
+	@GetMapping
+	public String test(){
+		return "TEST-SERVICE";
+	}
+
+	@GetMapping(path = "/test")
+	public String testTest(){
+		return "TEST-SERVICE";
+	}
+
+}
